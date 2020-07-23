c36fd24ffec9745ebd4a62ee4281fa8141045982
Artem033030 <apilipcuk114@gmail.com>
2020-07-23 12:43:56 +0300

    hm

---
+++ Functions/colk.py
@@ -0,0 +1,36 @@
+class calculator():
+
+    def _add(self, a, b):
+        self.a = a
+        self.b = b
+        return a + b
+
+    def _edd(self, a, b):
+        self.a = a
+        self.b = b
+        return a - b
+
+    def _odd(self, a, b):
+        self.a = a
+        self.b = b
+        return a * b
+
+    def _ad(self, a, b):
+        self.a = a
+        self.b = b
+        return a / b
+
+    def first_value(self, a):
+
+    def second_value(self, b):
+
+    def act(self, a, b):
+        rt = input("+ , - , * , /")
+        if rt == "+":
+            add(a, b)
+        if ot == "-":
+            edd(a, b)
+        if mt == "*":
+            odd(a, b)
+        if it == "/":
+            ad(a,b)
