e2869ed0c9ec995093bc41a4afa180f70b8906cc
Artem033030 <apilipcuk114@gmail.com>
2020-07-28 14:12:42 +0300

    hm

--- Functions/colk.py
+++ Functions/colk.py
@@ -1,9 +1,9 @@
 class Calculator:
 
     def __init__(self):
-        self.a = None
-        self.b = None
-        self.result = None
+        self.a = 0, 000
+        self.b = 0, 000
+        self.result = 0, 000
 
     def _add(self):
         return self.a + self.b
@@ -22,31 +22,44 @@ class Calculator:
         self.b = b
         if action == '+':
             self.result = self._add()
-            return self.result
 
         if action == '-':
             self.result = self._substr()
-            return self.result
 
         if action == '*':
             self.result = self._mult()
-            return self.result
 
         if action == '/':
             self.result = self._div()
-            return self.result
+        return self.result
 
 
 calc = Calculator()
-calc.act(2)
-calc.act(3)
-print(calc.act('+'))
-calc.act(2)
-calc.act(3)
-print(calc.act('-'))
-calc.act(2)
-calc.act(3)
-print(calc.act('*'))
-calc.act(2)
-calc.act(3)
-print(calc.act('/'))
+add = calc.act("+", 1.444, 2.555)
+print(add)
+# print(1)
+# if add == 3:
+#     print("done")
+# else:
+#     print('nope')
+# sub = calc.act("-", 1, 2)
+# print(2)
+# if sub == -1:
+#     print("done")
+# else:
+#     print('nope')
+# mult = calc.act("*", 1, 2)
+# print(3)
+# if mult == 2:
+#     print("done")
+# else:
+#     print('nope')
+# div = calc.act("/", 1, 2)
+# print(4)
+# if div == 0.5:
+#     print("done")
+# else:
+#     print('nope')
+
+
+# class thousandth(Calculator):
