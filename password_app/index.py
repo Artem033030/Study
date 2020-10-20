638b2da9474ad6dc3e9f56309897c8ae46f55487
Artem033030 <apilipcuk114@gmail.com>
2020-10-20 17:58:04 +0300

    hm

--- password_app/index.py
+++ password_app/index.py
@@ -2,35 +2,40 @@ print('Hello world')
 storage = {}
 
 
-def logic():
+class PasswordHandler:
 
-    action = input('What do you want to do (C,R,U,D): ').capitalize()
-    if action == 'C':
-        print('Create action selected')
+    def logic(self):
 
-    elif action == 'R':
-        print('Read action selected')
-    elif action == 'U':
-        print('Update action selected')
-    elif action == "D":
-        print('Delete action selected')
-    else:
-        print('Uoer answer is dont usalble')
+        action = input('What do you want to do (C,R,U,D): ').capitalize()
+        if action == 'C':
+            print('Create action selected')
+            self.creat_storage()
+            print(storage)
+        elif action == 'R':
+            print('Read action selected')
+            self.creat_storage()
+            print(storage)
+        elif action == 'U':
+            print('Update action selected')
+        elif action == "D":
+            print('Delete action selected')
+        else:
+            print('Uoer answer is dont usalble')
+
+    def creat_storage(self):
+
+        key = input('Please name of site: ')
+        password = input('Please password of site: ')
+        storage[key]= password
+
+    def del_storage(self):
+
+        delkey = input('Enter site: ')
+        delpassword = input('Enter password: ')
+        storage.pop[delkey]= delpassword
 
 
-def creat_storage():
 
-    key = input('Please name of site: ')
-    password = input('Please password of site: ')
-    storage[key]= password
-    print(storage)
-
-
-
-
-
-
-creat_storage()
 
 
 
---
+++ password_app/test.py
@@ -0,0 +1,3 @@
+from password_app.index import PasswordHandler
+handler = PasswordHandler()
+handler.logic()
